be75df60cb2a23ee6acfd44073b05e0d8a54e386
juniorjrml <jancarlolcj@gmail.com>
2022-10-14 00:19:50 -0300

    adicionado codigo html para a listagem adicionado relação do chirps para o usuario

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -14,6 +14,9 @@ class ChirpController extends Controller
      */
     public function index()
     {
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
         return view(
             'chirps.index',
             [
